7d64d3ccc8c75fcd9eed9038025d86d51964396c
Alexandre-Pascal <alexandre.pascal.ep@gmail.com>
2024-03-27 10:43:48 +0100

    Initialisation du repo, configuration, test Next.js

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <a href="/testationnage"> Testationnage </a>
+    </main>
+  );
+}
---
+++ app/testationnage/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Page() {
+  return (
+    <div>
+      <a href="../"><b>Testationnage</b></a>
+      <p>Ceci est une page de testationnage.</p>
+    </div>
+  );
+}
+
