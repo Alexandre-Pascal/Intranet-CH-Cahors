95d4d510811770b19977c3c2baf55794eb6fd510
Alexandre-Pascal <alexandre.pascal.ep@gmail.com>
2024-04-11 10:27:03 +0200

    Ajout de tous les icones pret à accueillir les liens

--- app/components/Header/NavBar/Menu/ListeTitresMenu.tsx
+++ app/components/Header/NavBar/Menu/ListeTitresMenu.tsx
@@ -1,6 +1,11 @@
 import { useState, useEffect } from "react";
 import { DataList } from "@/app/utils/types";
 import styles from "./styles.module.css";
+import Image from "next/image";
+
+import crayon from "../../../../assets/icons/crayon.png";
+import plus from "../../../../assets/icons/plus.png";
+import poubelle from "../../../../assets/icons/poubelle.png";
 
 export default function ListeTitres(datas : DataList[]) {
   const [dataList, setdataList] = useState<DataList[]>([]);
@@ -17,16 +22,21 @@ export default function ListeTitres(datas : DataList[]) {
     <div className={styles.menu}>
       <div className={styles.list_main_categories}>
         {Object.keys(dataList) && Object.values(dataList).map((category, index) => (
-          <h1 key={index} className=
-            {
-              selectedCategoryId === category.category_id - 1 ?
-                styles.selectedCategory : styles.notSelectedCategory
-            }
-          onClick={() => setSelectedCategoryId(category.category_id - 1)
-          }>
-            {category.category_name}
-          </h1>
+          <div className={styles.action_list}>
+            <h1 key={index} className=
+              {
+                selectedCategoryId === category.category_id - 1 ?
+                  styles.selectedCategory : styles.notSelectedCategory
+              }
+            onClick={() => setSelectedCategoryId(category.category_id - 1)
+            }>
+              {category.category_name}
+            </h1>
+            <a href=""><Image className={styles.icon_action_list} src={crayon} alt="crayon" width={32} height={32}/></a>
+            <a href=""><Image className={styles.icon_action_list} src={poubelle} alt="poubelle" width={32} height={32}/></a>
+          </div>
         ))}
+        <a href=""><Image className={styles.icon_action_list} style={{ marginInlineStart : "2vw" }} src={plus} alt="plus" width={32} height={32}/></a>
       </div>
       <div className={styles.list_sub_categories_and_titles}>
         <ul>
@@ -34,28 +44,38 @@ export default function ListeTitres(datas : DataList[]) {
           && dataList[selectedCategoryId]?.sub_categories?.map((subCategory, index) =>
             (
               <li key={index} className={styles.sub_categorie}>
-                {subCategory.sub_category_url ? (
-                  <a href={subCategory.sub_category_url}>
+                <div className={styles.action_list}>
+                  {subCategory.sub_category_url ? (
+                    <a href={subCategory.sub_category_url}>
+                      <h2>{subCategory.sub_category_name}</h2>
+                    </a>) : (
                     <h2>{subCategory.sub_category_name}</h2>
-                  </a>) : (
-                  <h2>{subCategory.sub_category_name}</h2>
-                )
-                }
+                  )
+                  }
+                  <a href=""><Image className={styles.icon_action_list} src={crayon} alt="crayon" width={20} height={20}/></a>
+                  <a href=""><Image className={styles.icon_action_list} src={plus} alt="plus" width={20} height={20} /></a>
+                  <a href=""><Image className={styles.icon_action_list} src={poubelle} alt="poubelle" width={20} height={20} /></a>
+                </div>
                 <ul>
                   {subCategory.titles.map((title, index) => (
                     <li>
-                      {title.title_url ? (
-                        <a href={title.title_url}>
+                      <div className={styles.action_list}>
+                        {title.title_url ? (
+                          <a href={title.title_url}>
+                            <h3 key={index}>{title.title_name}</h3>
+                          </a> ) : (
                           <h3 key={index}>{title.title_name}</h3>
-                        </a> ) : (
-                        <h3 key={index}>{title.title_name}</h3>
-                      )
-                      }
+                        )
+                        }
+                        <a href=""><Image className={styles.icon_action_list} src={crayon} alt="crayon" width={20} height={20}/></a>
+                        <a href=""><Image className={styles.icon_action_list} src={poubelle} alt="poubelle" width={20} height={20}/></a>
+                      </div>
                     </li>
                   ))}
                 </ul>
               </li>
             ))}
+          <a href=""><Image className={styles.icon_action_list} src={plus} alt="plus" width={32} height={32}/></a>
         </ul>
       </div>
     </div>
