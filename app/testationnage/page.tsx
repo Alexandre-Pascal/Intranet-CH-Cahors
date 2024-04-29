3a26feaaef7394cd52b2955e4bae230a2442b5fc
Alexandre-Pascal <alexandre.pascal.ep@gmail.com>
2024-04-29 13:55:02 +0200

    téléchargement des fichiers uploadé

--- app/testationnage/page.tsx
+++ app/testationnage/page.tsx
@@ -1,42 +1,28 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import FileList from "./FileList";
 
-export default function UploadForm() {
-  const [selectedFile, setSelectedFile] = useState(null);
+const DownloadPage: React.FC = () => {
+  const [files, setFiles] = useState<string[]>([]);
 
-  const handleFileChange = (event) => {
-    setSelectedFile(event.target.files[0]);
-  };
+  useEffect(() => {
+    fetch("/api/articles/download-file")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Erreur lors de la récupération des fichiers");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setFiles(data.files);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
 
-  const handleUpload = async() => {
-    if (!selectedFile) {
-      console.error("No file selected");
-      return;
-    }
+  return <FileList files={files} />;
+};
 
-    const formData = new FormData();
-    formData.append("files", selectedFile);
-
-    try {
-      const requestOptions = {
-        method: "POST",
-        body: formData,
-      };
-      const response = await fetch("/api/articles/upload-file", requestOptions);
-      const result = await response.json();
-      console.log(result);
-    } catch (error) {
-      console.error("Error uploading file:", error);
-    }
-  };
-
-  return (
-    <div>
-      <input type="file" onChange={handleFileChange} />
-      <button onClick={handleUpload}>Upload</button>
-      <FileList />
-    </div>
-  );
-}
+export default DownloadPage;
