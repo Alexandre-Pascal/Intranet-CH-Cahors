113e1a91e7cfc7edbca22e821feeb4e5bc0a8376
Alexandre-Pascal <alexandre.pascal.ep@gmail.com>
2024-05-02 10:12:59 +0200

    Réglage bug liste fichiers qui apparait plus après avoir fermer la liste

--- app/PageEditor/Editor/FilesManager/FilesManager.tsx
+++ app/PageEditor/Editor/FilesManager/FilesManager.tsx
@@ -15,26 +15,25 @@ export default function FilesManager({ idPage, onlyView }: FilesManagerProps) {
   const [ isOpenFileManager, setIsOpenFileManager ] = useState(false);
   return (
     <>
-      { !isOpenFileManager ? (
+      { !isOpenFileManager && (
         <button type="button"
           onClick={() => setIsOpenFileManager(true)}
           className={`bg-stone-300 hover:bg-stone-500 text-white font-bold py-2 px-4 rounded mt-4 ${styles.button_open_file_manager}`}
         >
           {onlyView ? "Voir les fichiers" : "Gestion des fichiers"}
         </button>
-      ) : (
-        <div className={styles.container_download_upload}>
-          <a className={styles.set_file_manager_close} onClick={() => setIsOpenFileManager(false)}><Icon name="ArrowLeftFromLine" /></a>
-          {!onlyView &&
+      )}
+      <div className={styles.container_download_upload} hidden={!isOpenFileManager}>
+        <a className={styles.set_file_manager_close} onClick={() => setIsOpenFileManager(false)}><Icon name="ArrowLeftFromLine" /></a>
+        {!onlyView &&
           <>
             <h2 className="text-center">Gestion des fichiers</h2>
             <UploadComponent idPage={idPage} setIsUpToDate={setIsUpToDate} />
           </>
-          }
-          <DownloadComponent idPage={idPage} isUpToDate={isUpToDate} setIsUpToDate={setIsUpToDate}/>
-        </div>
-      )}
-    </>
+        }
+        <DownloadComponent idPage={idPage} isUpToDate={isUpToDate} setIsUpToDate={setIsUpToDate}/>
+      </div>
 
+    </>
   );
 }
